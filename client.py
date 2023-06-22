40720c50c7a9f27c55732844ac132d5548735bf9
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-06-22 10:28:07 +0200

    refactor: combine the prints into one print out message

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
 your_name =  input("enter your name:")
-print(f"your name is {your_name}")
 my_age = int(input("enter your age: " ))
-print(f"your age is {my_age}")
+print(f"your age is {my_age} and your name {my_name}")
