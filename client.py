6638d64ff30b1d7ab936a8dae667dbfa3d299b45
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-06-22 11:51:39 +0200

    feat: add girl array changes coming from booking branch stashed

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
 your_name =  input("enter your name:")
 my_age = int(input("enter your age: " ))
-print(f"your age is {my_age} and your name {my_name}")
+girl = [1, 2, 3, 4, 5]
+print(f"your age is {my_age} and your name {your_name}")
