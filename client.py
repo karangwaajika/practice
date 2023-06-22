28f5c50b590e30cdaf5f1f15c19c2e6d6ee83e8c
Karangwa Ajika <52496651+karangwaajika@users.noreply.github.com>
2023-06-22 11:23:18 +0200

    edit the my_name to your_name

--- client.py
+++ client.py
@@ -1,3 +1,3 @@
 your_name =  input("enter your name:")
 my_age = int(input("enter your age: " ))
-print(f"your age is {my_age} and your name {my_name}")
+print(f"your age is {my_age} and your name {your_name}")
